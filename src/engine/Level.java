64425f700c535a753b39e14f0b3c33a73ee5f7f0
Bojia Chen <bojia.chen@duke.edu>
2015-03-28 22:44:08 -0400

    Started on Level class

--- src/engine/Level.java
+++ src/engine/Level.java
@@ -1,14 +1,40 @@
 package engine;
 
-/*
- * Contains all information about a single level within a game.  Has methods for checking win/loss conditions, updating the scene, and 
- * moving between rounds and waves.  The level also contains a pointer to another level which will be called if the win conditions are met -
- * if the loss conditions are met, a generic "Game Over" is loaded before the same level is reloaded.
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 
+ * Contains all information about a single level within a game. Has methods for checking
+ * win/loss conditions, updating the scene, and
+ * moving between rounds and waves. The level also contains a pointer to another level which
+ * will be called if the win conditions are met -
+ * if the loss conditions are met, a generic "Game Over" is loaded before the same level is
+ * reloaded.
+ * 
+ * @author Bojia Chen
+ * 
  */
+
+// TODO: Win/Lose Conditions
+
 public class Level {
 
-	public void update(){
-		
-	}
-	
+    private List<Round> myRounds;
+    private double health;
+    private int lives;
+
+    public Level () {
+        myRounds = new ArrayList<>();
+    }
+
+    /**
+     * Updates all game objects/parameters to advance gameplay.
+     */
+
+    public void update () {
+
+    }
+
 }
