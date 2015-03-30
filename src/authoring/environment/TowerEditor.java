2cd9ba43bbe111600c61a78fb80aceee9c4fb4b8
Qian Wang <qwang2012@gmail.com>
2015-03-30 16:03:21 -0400

    Fix prior trivial merge conflicts

--- src/authoring/environment/TowerEditor.java
+++ src/authoring/environment/TowerEditor.java
@@ -11,10 +11,11 @@ import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 import authoring.environment.objects.Tower;
 
-public class TowerEditor extends PropertyEditor{
+
+public class TowerEditor extends PropertyEditor {
     private Group myRoot;
-    
-    public ArrayList<Tower> getTowers(){
+
+    public ArrayList<Tower> getTowers () {
         return new ArrayList<>();
     }
 
@@ -23,32 +24,36 @@ public class TowerEditor extends PropertyEditor{
         // TODO Auto-generated method stub
         myRoot = new Group();
         StackPane content = new StackPane();
-        
+
         // TODO remove magic number
-        Rectangle background = new Rectangle(MainEnvironment.myDimensions.getWidth(), 0.9 * MainEnvironment.myDimensions.getHeight(), Color.BLUE);
-        
-        // TODO remove magic numbers 
+        Rectangle background =
+                new Rectangle(MainEnvironment.myDimensions.getWidth(),
+                              0.9 * MainEnvironment.myDimensions.getHeight(), Color.BLUE);
+
+        // TODO remove magic numbers
         HBox row = new HBox(10);
         row.setAlignment(Pos.TOP_CENTER);
-        Rectangle tower = new Rectangle(40,40,Color.WHITE); //hard-coded placeholders
-        tower.setOnMousePressed((e) -> handleTowerEdit());
-        Rectangle tower2 = new Rectangle(40,40,Color.WHITE);
-        Rectangle tower3 = new Rectangle(40,40,Color.WHITE);
+        Rectangle tower = new Rectangle(40, 40, Color.WHITE); // hard-coded placeholders
+        tower.setOnMousePressed( (e) -> handleTowerEdit());
+        Rectangle tower2 = new Rectangle(40, 40, Color.WHITE);
+        Rectangle tower3 = new Rectangle(40, 40, Color.WHITE);
         row.getChildren().addAll(tower, tower2, tower3);
-        
+
         content.getChildren().addAll(background, row);
         myRoot.getChildren().addAll(content);
         return myRoot;
     }
-    
-    private void handleTowerEdit(){
-        Rectangle overlay = new Rectangle(MainEnvironment.myDimensions.getWidth(), 0.9 * MainEnvironment.myDimensions.getHeight());
+
+    private void handleTowerEdit () {
+        Rectangle overlay =
+                new Rectangle(MainEnvironment.myDimensions.getWidth(),
+                              0.9 * MainEnvironment.myDimensions.getHeight());
         overlay.setOpacity(0.6);
         myRoot.getChildren().add(overlay);
         showEditScreen(overlay);
     }
-    
-    private void showEditScreen(Rectangle overlay){
+
+    private void showEditScreen (Rectangle overlay) {
         ScaleTransition scale = new ScaleTransition(Duration.millis(500), overlay);
         scale.setFromX(0.0);
         scale.setFromY(0.0);
@@ -57,13 +62,4 @@ public class TowerEditor extends PropertyEditor{
         scale.setCycleCount(1);
         scale.play();
     }
-<<<<<<< HEAD
-    
-    
-
-=======
->>>>>>> 0e1e78ec9d85c9d4d0619f71b0667eeb61faa7ec
 }
-
-
-
