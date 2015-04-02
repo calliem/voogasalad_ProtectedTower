84eef1680aed0699ac0dd57b54aa72eb727912f1
Bojia Chen <bojia.chen@duke.edu>
2015-04-02 12:05:37 -0400

    started TowerManager and TowerNode classes

---
+++ src/engine/TowerNode.java
@@ -0,0 +1,14 @@
+package engine;
+
+/**
+ * This object is the node that is added to the tower trees in TowerManager. It
+ * holds the specific tower object itself, the next node, and method for getting
+ * new instances of the tower object.
+ * 
+ * @author Bojia Chen
+ *
+ */
+
+public class TowerNode {
+
+}
