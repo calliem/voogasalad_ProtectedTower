342d25107366ffd995cc60befa2865f5bccab00d
Bojia Chen <bojia.chen@duke.edu>
2015-04-02 23:30:26 -0400

    Added equals and hashcode to TowerManager

--- src/engine/TowerNode.java
+++ src/engine/TowerNode.java
@@ -16,19 +16,25 @@ import engine.sprites.Tower;
 public class TowerNode {
 
 	private String myName;
+	private String myGroup;
 	private Tower myTower;
 	private Set<TowerNode> myNextNodes;
 	private Set<TowerNode> myPrevNodes;
 
-	public TowerNode(String name, Tower tower) {
+	public TowerNode(String name, String group, Tower tower) {
 		myName = name;
+		myGroup = group;
 		myTower = tower.clone();
 	}
-	
+
 	protected String getName() {
 		return myName;
 	}
-	
+
+	protected String getGroup() {
+		return myGroup;
+	}
+
 	protected Tower getTower() {
 		return myTower.clone();
 	}
@@ -55,5 +61,24 @@ public class TowerNode {
 
 	protected boolean removePrevNode(TowerNode node) {
 		return myPrevNodes.remove(node);
+	}
+
+	/**
+	 * Overriding hashcode and equals
+	 */
+
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof TowerNode))
+			return false;
+		if (o == this)
+			return true;
+		TowerNode node = (TowerNode) o;
+		return myName.equals(node.getName()) && myGroup.equals(node.getGroup());
+	}
+
+	@Override
+	public int hashCode() {
+		return (myName + myGroup).hashCode();
 	}
 }
