b0feef515557bc1ad99c46cd570feaff6d7e5e16
Callie Mao <callie.mao@gmail.com>
2015-04-08 10:17:36 -0400

    Created aabstract class for the wave editor

--- src/authoringEnvironment/editors/WaveEditor.java
+++ src/authoringEnvironment/editors/WaveEditor.java
@@ -7,13 +7,18 @@ import javafx.scene.Node;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import authoringEnvironment.MainEnvironment;
 import authoringEnvironment.objects.FlowView;
 import authoringEnvironment.objects.UnitView;
 
 public class WaveEditor extends MainEditor {
 	private Group myRoot;
-
+	private static final double CONTENT_WIDTH = MainEnvironment.getEnvironmentWidth();
+    private static final double CONTENT_HEIGHT = 0.89 * MainEnvironment.getEnvironmentHeight();
+    
 	public WaveEditor(Dimension2D dim, Stage s) {
 		super(dim, s);
 	}
@@ -21,6 +26,8 @@ public class WaveEditor extends MainEditor {
 	@Override
 	public Node configureUI() {
 		myRoot = new Group();		
+		
+		Rectangle background = new Rectangle(CONTENT_WIDTH, CONTENT_HEIGHT, Color.GRAY);
 		
 		HBox wave1 = makeNewWave();
 		myRoot.getChildren().add(wave1);
