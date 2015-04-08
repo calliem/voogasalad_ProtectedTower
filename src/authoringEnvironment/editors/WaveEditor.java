31446547869dfec92fc48680bbdc7a858d70f08a
Megan Gutter <megan.gutter@duke.edu>
2015-04-08 02:56:02 -0400

    ability to add new waves and add units to waves

--- src/authoringEnvironment/editors/WaveEditor.java
+++ src/authoringEnvironment/editors/WaveEditor.java
@@ -1,19 +1,31 @@
 package authoringEnvironment.editors;
 
 import java.util.ArrayList;
+
 import javafx.geometry.Dimension2D;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import authoringEnvironment.objects.FlowView;
 import authoringEnvironment.objects.UnitView;
 
+/**
+ * Creates the Wave Editor that allows the user to create
+ * and edit waves made out of units or other waves.
+ *  
+ * @author Megan Gutter
+ *
+ */
+
 public class WaveEditor extends MainEditor {
 	private Group myRoot;
-
+	private ArrayList<String> myWaves;
 
 	public WaveEditor(Dimension2D dim, Stage s) {
 		super(dim, s);
@@ -22,25 +34,51 @@ public class WaveEditor extends MainEditor {
 	@Override
 	public Node configureUI() {
 		myRoot = new Group();		
+		HBox newWavePanel = new HBox(10);
+		VBox contents = new VBox(10);
 		
-		HBox wave1 = makeNewWave();
-		myRoot.getChildren().add(wave1);
+		Button makeNewWave = new Button("Create New Wave");
+		makeNewWave.setOnAction(e -> {
+			makeNewWave(contents);
+		});
+		
+		newWavePanel.getChildren().add(makeNewWave);
+		contents.getChildren().add(newWavePanel);
+		
+		myRoot.getChildren().add(contents);
 		return myRoot;
 	}
 	
-	private HBox makeNewWave() {
-		HBox newWave = new HBox(10);
+	private void makeNewWave(VBox contents) {
+		ScrollPane newWave = new ScrollPane();
+		newWave.setHbarPolicy(ScrollBarPolicy.ALWAYS);
+		newWave.setVbarPolicy(ScrollBarPolicy.NEVER);
+		
+		HBox waveContent = new HBox(10);
+
 		Button addUnit = new Button("Add Unit");
 		addUnit.setOnAction(e -> {
-			addUnitToWave(newWave);
+			addUnitToWave(waveContent);
 		});
-		addUnit.setAlignment(Pos.TOP_RIGHT);
-		newWave.getChildren().add(addUnit);
-		return newWave;
+		
+		Button save = new Button("Save");
+		save.setOnAction(e -> {
+			
+		});
+		
+		VBox buttons = new VBox(10);
+		buttons.getChildren().add(addUnit);
+		buttons.getChildren().add(save);
+		
+		waveContent.getChildren().add(buttons);
+		newWave.setContent(waveContent);
+		
+		contents.getChildren().add(newWave);
+		//return newWave;
 	}
 	
 	private void addUnitToWave(HBox wave) {
-		wave.getChildren().add(new FlowView(100, 100));
+		wave.getChildren().add(new FlowView(100, 100));		
 	}
 
 	public ArrayList<UnitView> getWaves() {
