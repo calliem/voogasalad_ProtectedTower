cc0c28e864c5a740f2931ee8bb6c8f5a8d33f0cb
Megan Gutter <megan.gutter@duke.edu>
2015-04-24 00:15:21 -0400

    created RoundEditor class

---
+++ src/authoringEnvironment/editors/RoundEditor.java
@@ -0,0 +1,20 @@
+package authoringEnvironment.editors;
+
+import authoringEnvironment.Controller;
+
+
+/**
+ * Sets up the round editor that allows the user to pick a map and designate spawn points for
+ * selected waves of units that will spawn at one time in a level
+ * 
+ * @author Megan Gutter
+ */
+public class RoundEditor extends MainEditor {
+
+    public RoundEditor (Controller c, String name) {
+        super(c, name);
+    }
+    
+    
+
+}
