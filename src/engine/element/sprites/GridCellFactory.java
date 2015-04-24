8443561e62577dc28a51c717f4392634ba606a91
Qian Wang <qwang2012@gmail.com>
2015-04-24 01:45:04 -0400

    Recoded GameMap, made GridCellFactory extend SpriteFactory

--- src/engine/element/sprites/GridCellFactory.java
+++ src/engine/element/sprites/GridCellFactory.java
@@ -1,7 +1,6 @@
 package engine.element.sprites;
 
 import java.security.InvalidParameterException;
-import java.util.HashMap;
 import java.util.Map;
 import util.reflection.Reflection;
 
@@ -12,35 +11,13 @@ import util.reflection.Reflection;
  * @author Qian Wang
  *
  */
-public class GridCellFactory {
+public class GridCellFactory extends SpriteFactory {
 
     private Map<String, Map<String, Object>> myGridCells;
     private final static String MY_CLASS_NAME = "engine.element.sprites.GridCell";
 
     public GridCellFactory () {
-        myGridCells = new HashMap<>();
-    }
-
-    /**
-     * Adds new grid cells to the list of all possible grid cells, can be called with a map of
-     * grid cells GUID to the parameters map of that grid cells, or also with a single GUID and a
-     * single parameters map.
-     * 
-     * @param allSprites Map<String, Map<String, Object>> object
-     */
-    public void add (Map<String, Map<String, Object>> allSprites) {
-        // TODO refactor into superclass for factories
-        allSprites.keySet().forEach(t -> this.add(t, allSprites.get(t)));
-    }
-
-    /**
-     * @see GridCellFactory#add(Map)
-     * 
-     * @param gridcellID String of the GUID of the projectile
-     * @param gridcellProperties the properties Map<String, Object> object of the projectile
-     */
-    public void add (String gridcellID, Map<String, Object> gridcellProperties) {
-        myGridCells.put(gridcellID, gridcellProperties);
+        super(MY_CLASS_NAME);
     }
 
     /**
