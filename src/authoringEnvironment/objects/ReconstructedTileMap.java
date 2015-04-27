170ad0566111b8eda4cf6cffeb3fae6ea76923b8
Callie Mao <callie.mao@gmail.com>
2015-04-27 05:26:52 -0400

    Created reconstructed tile map

---
+++ src/authoringEnvironment/objects/ReconstructedTileMap.java
@@ -0,0 +1,52 @@
+package authoringEnvironment.objects;
+
+import java.util.Map;
+import voogasalad.util.IntArray2DToImageConverter.src.IntArray2DToImageConverter;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import authoringEnvironment.Controller;
+import authoringEnvironment.InstanceManager;
+import authoringEnvironment.Variables;
+
+
+public class ReconstructedTileMap extends TileMap {
+
+    private Controller myController;
+
+    public ReconstructedTileMap (Controller c, String key) {
+        // super class
+        myController = c;
+
+        Map<String, Object> params = myController.getPartCopy(key);
+        String name = (String) params.get(InstanceManager.NAME_KEY);
+        setName(name);
+
+        int tileSize = (int) params.get(Variables.PARAMETER_TILESIZE);
+        setTileSize(tileSize);
+
+        String filePath = (String) params.get(Variables.PARAMETER_BACKGROUND_FILEPATH);
+        setBackground(new ImageView(new Image(filePath)));
+
+        int[][] thumbnailArray = (int[][]) params.get(Variables.PARAMETER_THUMBNAIL);
+        Image thumbnail = IntArray2DToImageConverter.convert2DIntArrayToImage(thumbnailArray, 1);
+        setThumbnail(new ImageView(thumbnail));
+        // setFilePath()
+
+        createMap(params);
+        changeTileSize(getTileSize());
+        //createGridLines();
+    }
+
+    private void createMap (Map<String, Object> params) {
+        String[][] tileArray = (String[][]) params.get(TILE_KEY_ARRAY);
+        setTiles(new Tile[tileArray[0].length][tileArray.length]);
+        for (int i = 0; i < tileArray[0].length; i++) {
+            for (int j = 0; j < tileArray.length; j++) {
+                myTiles[i][j] = new Tile(myController, tileArray[i][j]);  // TODO: make tileview
+                myTiles[i][j].positionTile(getTileSize(), i, j);
+                getRoot().getChildren().add(myTiles[i][j]);
+                setupTooltip(myTiles[i][j]);
+            }
+        }
+    }
+}
--- src/authoringEnvironment/objects/Sidebar.java
+++ src/authoringEnvironment/objects/Sidebar.java
@@ -1,22 +1,17 @@
 package authoringEnvironment.objects;
 
 import java.util.ResourceBundle;
-import javafx.animation.PauseTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.util.Duration;
 import authoringEnvironment.AuthoringEnvironment;
 import authoringEnvironment.map.MapWorkspace;
 
@@ -63,15 +58,9 @@ public abstract class Sidebar extends VBox { // extend gridpane pls
         this.getChildren().add(topContext);
         this.getChildren().add(accordionContext);
         setContent(topContext);
-        // setSpacing(10);
-        // createMapSettings();
     }
 
     protected abstract void setContent (GridPane container);
-
-    // protected GridPane getTopContext(){
-    // return topContext;
-    // }
 
     protected MapWorkspace getMapWorkspace () {
         return myMapWorkspace;
@@ -96,20 +85,6 @@ public abstract class Sidebar extends VBox { // extend gridpane pls
         accordionContext.getPanes().add(organizer);
         return context;
     }
-    
-  /*  protected void displayWorkspaceMessage(String text, Color color){
-        Text saved = new Text(text);
-        saved.setFill(color);
-        saved.setFont(new Font(20));
-        StackPane.setAlignment(saved, Pos.BOTTOM_CENTER);
-        //saved.setVisible(false);
-        myMapWorkspace.getChildren().add(saved);
-        
-        //saved.setVisible(true);
-        PauseTransition pause = new PauseTransition(Duration.millis(1000));
-        pause.play();
-        pause.setOnFinished(e ->getMapWorkspace().getChildren().remove(saved));
-    }*/
 
     protected ListView<Node> createListView (ObservableList<Node> items, int height) {
         ListView<Node> list = new ListView<Node>();
