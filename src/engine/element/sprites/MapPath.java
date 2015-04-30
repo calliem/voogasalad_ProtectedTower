a4c0d508c2aae5f424651591f5909a0b91478854
Qian Wang <qwang2012@gmail.com>
2015-04-29 21:43:03 -0400

    Add method to add instance variables to path

--- src/engine/element/sprites/MapPath.java
+++ src/engine/element/sprites/MapPath.java
@@ -1,6 +1,7 @@
 package engine.element.sprites;
 
 import java.util.List;
+import java.util.Map;
 import annotations.parameter;
 import authoringEnvironment.objects.Coordinate;
 
@@ -9,6 +10,14 @@ public class MapPath extends GameElement {
 
     @parameter(settable = true, playerDisplay = false, defaultValue = "null")
     private List<Coordinate> coordinates;
+
+    public MapPath () {
+
+    }
+
+    public void addInstanceVariables (Map<String, Object> parameters) {
+        // TODO fill out this method
+    }
 
     @Override
     public void onCollide (GameElement element) {
